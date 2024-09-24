cf97ce70d23ea46d25622cf950b1fe006be233a3
Bui Tien Anh <anhbthe170035@fpt.edu.vn>
2024-09-24 23:17:31 +0700

    Fixing login, register screens

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -18,7 +18,7 @@ public class DBContext {
     public DBContext() {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP391";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=mot";
             String username = "sa";
             String password = "sa";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
